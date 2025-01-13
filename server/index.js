d58c2efa85e90b6db8479ee55dbf60419db66c58
shankarjatin <shankarjatin1005@gmail.com>
2025-01-14 04:34:44 +0530

    inndex with load balancer

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const cluster = require('cluster');
+const os = require('os');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api', userRoutes);
+
+// Handle non-existing endpoints
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+
+if (cluster.isMaster) {
+  const numWorkers = os.cpus().length;
+  console.log(`Master is running. Forking ${numWorkers} workers.`);
+
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
