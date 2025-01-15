85b8867d0d17150699624415f793d80645c3bccf
shankarjatin <shankarjatin1005@gmail.com>
2025-01-15 06:16:07 +0530

    listing done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import MainVisualization from "./components/MainVisualization";
 import Sidebar from "./components/Sidebar";
 import UserForm from "./components/UserForm";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+import { Menu } from "lucide-react";
 
 const App = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <Provider store={store}>
-      <div className="flex flex-col lg:flex-row h-screen overflow-auto bg-[#1E1E2F]">
+      <div className="flex h-screen bg-[#1E1E2F]">
+        {/* Hamburger Menu */}
+        <div className="lg:hidden p-4">
+          <button
+            onClick={toggleSidebar}
+            className="text-[#FFFFFF] bg-[#2A2A40] p-2 rounded-md focus:outline-none"
+          >
+            <Menu size={24} />
+          </button>
+        </div>
+
         {/* Sidebar */}
-        <Sidebar className="h-full sticky top-0 lg:h-screen flex-shrink-0 lg:w-1/4 w-full" />
+        {isSidebarOpen && (
+          <Sidebar className="h-full sticky top-0 lg:h-screen flex-shrink-0 lg:w-1/4 w-full" />
+        )}
 
         {/* Main Content */}
         <div className="flex flex-1 flex-col">
@@ -26,6 +47,17 @@ const App = () => {
           </div>
         </div>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </Provider>
   );
 };
